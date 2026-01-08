bfc1ad99b069b6149a7bb0f0962a60c99d510fc8
rakshitakampli36-art <rakshitakampli36@gmail.com>
2026-01-08 20:25:42 +0530

    Add files via upload

---
+++ identit_operator.py
@@ -0,0 +1,6 @@
+lst_1=list(map(int,input("enter a list").split()))
+lst_2=list(map(int,input("enter a list").split()))
+if lst_1 is lst_2:
+    print("both list are same")
+else:
+    print("both list are not same")
