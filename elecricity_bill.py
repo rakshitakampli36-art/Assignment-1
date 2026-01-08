98926f16a91d9b0a07e3a23cc2604463952f6282
rakshitakampli36-art <rakshitakampli36@gmail.com>
2026-01-08 12:33:34 +0530

    Update elecricity_bill.py

--- elecricity_bill.py
+++ elecricity_bill.py
@@ -3,9 +3,9 @@ def electric_bill(units):
     if units<=100:
         bill=units*2
     elif units<=200:
-        bill=units*4
+        bill=(100*2)+((units -100)*4)
     elif units>200:
-        bill=units*6
+        bill=(100*2)+(100*4)+((units -200)*6)
     return bill
 units=int(input("eneter number of units "))
 totalbill=electric_bill(units)
