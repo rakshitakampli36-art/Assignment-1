5d5f74bcd3a35245c94027a7701b48cc37c9c83d
rakshitakampli36-art <rakshitakampli36@gmail.com>
2026-01-08 20:26:15 +0530

    Add files via upload

---
+++ logical_operator.py
@@ -0,0 +1,19 @@
+marks=float(input("enter marks of student: "))
+attendance=int(input("enter attendence percentage of student"))
+if marks>40 and attendance>75:
+    print("pass")
+else:
+    print("fail")
+
+
+given_email=dd@admin@gmail.com
+given_usename=digital@dream
+email=input("enter email")
+username=input("enter username")
+if given_email==email or given_usename==username:
+    print("Login successful")
+else:
+    print("Login denied")
+
+
+
