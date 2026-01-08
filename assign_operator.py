911344cafa1380f7c9fe4cbedef9d4e14bc03390
rakshitakampli36-art <rakshitakampli36@gmail.com>
2026-01-08 20:25:04 +0530

    Add files via upload

---
+++ assign_operator.py
@@ -0,0 +1,20 @@
+initial_wallet_bal=float(input("Enter initial wallet balance :"))
+bill=float(input("Enter amount of shopping bill :"))
+initial_wallet_bal-=bill
+print("wallet balance ater shhopping is :",initial_wallet_bal)
+
+
+
+init_score=int(input("Enter initial score"))
+inc_score=int(input("Enter score to be added"))
+init_score+=inc_score
+print("Score is :",init_score)
+
+no_stocks=int(input("Enter number of stocks"))
+sales_of_stocks=int(input("Enter number of sold stocks"))
+no_stocks-=sales_of_stocks
+print("Remaining stocks are: ",no_stocks)
+
+
+
+
