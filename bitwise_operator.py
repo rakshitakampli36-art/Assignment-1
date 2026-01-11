31c772b6cec98da87fae91c582a5735dc66a7f94
rakshitakampli36-art <rakshitakampli36@gmail.com>
2026-01-11 18:41:14 +0530

    Add files via upload

---
+++ bitwise_operator.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+print(" AND (&):",a&b)
+print(" OR (|):",a|b)
+
+num=int(input("enter a number for right shift"))
+result=num>>1
+print("Right shift : ",result)
+
+
+num2=int(input("enter a number for left shift"))
+resultt=num<<1
+print("left shift : ",resultt)
