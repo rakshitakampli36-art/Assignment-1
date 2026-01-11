bf4111310d2b101a446d58ec0131b2d161c07738
rakshitakampli36-art <rakshitakampli36@gmail.com>
2026-01-11 18:43:40 +0530

    Add files via upload

---
+++ dictionary_assign.py
@@ -0,0 +1,6 @@
+example={"Laptop":"Electronics","Chair":"Furniture","Mobile":"Electronics"}
+print(example)
+print("All products names are :",example.keys())
+print("All categories are :",example.values())
+print("Count total products  are :",len(example.keys()))
+
